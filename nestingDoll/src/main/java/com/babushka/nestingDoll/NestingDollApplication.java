a86d5335c37d62f6d496777ca70cd4a339f9693a
AlbinSjodin <albinsjodin@gmail.com>
2018-12-11 14:54:34 +0100

    Har skapat en springit fil för projektet

---
+++ nestingDoll/src/main/java/com/babushka/nestingDoll/NestingDollApplication.java
@@ -0,0 +1,12 @@
+package com.babushka.nestingDoll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NestingDollApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NestingDollApplication.class, args);
+	}
+}
---
+++ nestingDoll/src/test/java/com/babushka/nestingDoll/NestingDollApplicationTests.java
@@ -0,0 +1,16 @@
+package com.babushka.nestingDoll;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NestingDollApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
