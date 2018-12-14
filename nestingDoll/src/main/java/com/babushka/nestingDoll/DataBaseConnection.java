d1b1c8fa7490f202f0e8341acf8e7aadbcd10e27
LouiseHoll <louise.hollingworth@gmail.com>
2018-12-14 09:36:43 +0100

    jdbc ok och länkar fungerar via localhost som också fungerar

---
+++ nestingDoll/src/main/java/com/babushka/nestingDoll/DataBaseConnection.java
@@ -0,0 +1,26 @@
+package com.babushka.nestingDoll;
+
+import org.springframework.stereotype.Component;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+@Component
+public class DataBaseConnection {
+    public Connection con = connect("jdbc:mysql://localhost:3306/nestingdoll?useSSL=false&useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC");
+
+    private Connection connect(String url) {
+
+        System.out.println("Creating connection");
+        try {
+//            var connection = DriverManager.getConnection(url, "root", "Sigurd1121");
+            var connection = DriverManager.getConnection(url, "root", "1BROR2john");
+            System.out.println("Connected");
+            return connection;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
--- nestingDoll/src/main/java/com/babushka/nestingDoll/controller/FormController.java
+++ nestingDoll/src/main/java/com/babushka/nestingDoll/controller/FormController.java
@@ -1,13 +1,23 @@
 package com.babushka.nestingDoll.controller;
 
+import com.babushka.nestingDoll.DataBaseConnection;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class FormController {
 
+    @Autowired
+    DataBaseConnection repository;
+
     @GetMapping ("/")
     public String index(){
         return "index";
+    }
+
+    @GetMapping ("/nestingdoll")
+    public String nestingdoll(){
+        return "nestingdoll";
     }
 }
